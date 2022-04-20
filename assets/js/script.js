0b8de758cb466d675cb5285da7f875012932c3a5
Tung Nguyen <tung.nguyen@viivue.com>
2022-04-20 18:29:23 +0700

    Lesson 5: list rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
             href: 'https://vuejs.org/',
             inStock: true,
             inventory:0,
+            details: ['50% cottons', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color:'green'},
+                {id: 2235, color:'blue'}
+            ]
         }
     }
 })
